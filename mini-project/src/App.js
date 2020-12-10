d7c8d05a980c6f4f1b29f023944f830b509bdd51
leo <69598134+Leeeeeeeeeeeeeo@users.noreply.github.com>
2020-12-10 14:29:47 -0500

    first component

--- mini-project/src/App.js
+++ mini-project/src/App.js
@@ -1,16 +1,18 @@
 import React, { Component, createElement } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <p className="App-intro">
-    //     Anybody Home? no?
-    //     </p>
-    //   </div>
-    // );
-    return React.createElement('div', {className: 'App'}, createElement('h1',null, 'I need a null after any tap' ))
+    return (
+      <div className="App">
+        <p className="App-intro">
+        Anybody Home? no?
+        </p>
+        <Person />
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, createElement('h1',null, 'I need a null after any tap' ))
   }
 }
 
