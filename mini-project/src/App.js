a67931bfcb31805ebd15bfc0c6f6e64d1f4ed8de
leo <69598134+Leeeeeeeeeeeeeo@users.noreply.github.com>
2020-12-10 11:02:00 -0500

    React creat Element

--- mini-project/src/App.js
+++ mini-project/src/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component, createElement } from 'react';
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        
-        <p className="App-intro">
-        Anybody Home? no?
-        </p>
-      </div>
-    );
+    // return (
+    //   <div className="App">
+    //     <p className="App-intro">
+    //     Anybody Home? no?
+    //     </p>
+    //   </div>
+    // );
+    return React.createElement('div', {className: 'App'}, createElement('h1',null, 'I need a null after any tap' ))
   }
 }
 
