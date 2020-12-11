c6c082b728896c13c264cbf989ea4def780bbd6b
leo <69598134+Leeeeeeeeeeeeeo@users.noreply.github.com>
2020-12-10 23:04:00 -0500

    creating props

--- mini-project/src/App.js
+++ mini-project/src/App.js
@@ -3,13 +3,30 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Rod', age: 200}
+    ]
+  }
+
+  switchNameHandle = () => {
+    console.log('button clicked')
+    this.setState({
+      persons :[
+        {name: 'Rodddd', age: 2000}
+      ]
+    })
+  }
+  
   render() {
     return (
       <div className="App">
         <p className="App-intro">
         Anybody Home? no?
         </p>
-        <Person />
+        <button onClick = {this.switchNameHandle}>Switch Name</button>
+        <Person name= {this.state.persons[0].name} age = {this.state.persons[0].age}> I'm not on earth </Person>
+       
       </div>
     );
     // return React.createElement('div', {className: 'App'}, createElement('h1',null, 'I need a null after any tap' ))
