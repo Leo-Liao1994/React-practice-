f34d07bc54da19e4476d2959e7bd66dc384a04cf
leo <69598134+Leeeeeeeeeeeeeo@users.noreply.github.com>
2020-12-11 21:59:45 -0500

    changing from class base to functional base

--- mini-project/src/App.js
+++ mini-project/src/App.js
@@ -1,35 +1,45 @@
-import React, { Component, createElement } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-class App extends Component {
-  state = {
-    persons: [
-      {name: 'Rod', age: 200}
+const app = (props) => {
+ 
+  const [personsState, setPersonState ] =  useState({ // [0] is the state  [1] is the function that motify state
+    persons: [    
+    {name: 'Rod', age: 200},
+    {name: 'Hoff', age: 300},
+    {name: 'Lee', age: 412}
     ]
-  }
+  })
 
-  switchNameHandle = () => {
+
+  const switchNameHandle = () => {
     console.log('button clicked')
-    this.setState({
+    setPersonState({
       persons :[
-        {name: 'Rodddd', age: 2000}
+        {name: 'Rodddd', age: 2000},
+        {name: 'Hoff', age: 300},
+        {name: 'Lee', age: 412}
       ]
     })
   }
+
   
-  render() {
     return (
       <div className="App">
         <p className="App-intro">
         Anybody Home? no?
         </p>
-        <button onClick = {this.switchNameHandle}>Switch Name</button>
-        <Person name= {this.state.persons[0].name} age = {this.state.persons[0].age}> I'm not on earth </Person>
-       
+        <button onClick = {switchNameHandle}>Switch Name</button>
+        <Person name= {personsState.persons[0].name} age = {personsState.persons[0].age}> I'm not on earth </Person>
+        <Person name= {personsState.persons[1].name} age = {personsState.persons[1].age}>  </Person>
+        <Person name= {personsState.persons[2].name} age = {personsState.persons[2].age}>  </Person>
+
       </div>
     );
   }
-}
 
-export default App;
+
+export default app;
+
+
